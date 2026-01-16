d836f1e5c9bed2e6f83162e09abcfc554a511d32
EnablistarRiddheshP <riddhesh.pingle@enablistar.com>
2026-01-16 22:47:52 +0530

    Commit: Fixing vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,29 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 
-/// https://vitejs.prod/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/kodingkraze6-dashboard-903239c8/",
-});
+// /// https://vitejs.prod/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/kodingkraze6-dashboard-903239c8/",
+// });
 
 // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:8081',
-//         changeOrigin: true,
-//         secure: false,
-//       }
-//     },
-//   },
-//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        secure: false,
+      }
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
